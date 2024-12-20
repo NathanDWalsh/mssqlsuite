527138aadde5c5158a7ffe10bf19e503413e642f
NathanDWalsh <12351394+NathanDWalsh@users.noreply.github.com>
2024-12-20 06:03:37 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -102,8 +102,8 @@ if ("sqlclient" -in $Install) {
     }
 
     if ($islinux) {
-        pwd
-        bash ./install_mssql_odbc18_ubuntu.sh
+        ls
+        bash /home/runner/work/els/els/install_mssql_odbc18_ubuntu.sh
     }
 
     Write-Output "sqlclient tools are installed"
