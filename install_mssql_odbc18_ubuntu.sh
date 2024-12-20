6683c3b666ef57cae0ebc1b405ed6acc82e04f1c
NathanDWalsh <12351394+NathanDWalsh@users.noreply.github.com>
2024-12-20 05:56:06 +0100

    Update install_mssql_odbc18_ubuntu.sh

--- install_mssql_odbc18_ubuntu.sh
+++ install_mssql_odbc18_ubuntu.sh
@@ -22,10 +22,5 @@ curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list |
 
 # Install the driver
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18 mssql-tools18 unixodbc-dev
+export PATH="$PATH:/opt/mssql-tools18/bin"
